7f45ee5b66e89fc7e4831c60395e5dd58a30d3f3
Nathan Chuluda <nchuluda@gmail.com>
2020-01-18 11:43:48 -0500

    Halfway thru Task 2

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, ModalFooter, Col, Row, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm } from 'react-redux-form';
+
 
 
 function RenderCampsite({campsite}) {
@@ -21,14 +23,22 @@ function RenderComments({comments}) {
         if (comments) {
             return(
                     <div className="col-md-5 m-1">
+                        <div className="row">
                         <h4>Comments</h4>
+                        </div>
+                        <div className="row">
                         {comments.map(comment => {
                             return(
                                 <div key={comment.id}>
                                     <p>{comment.text}<br />{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                    
                                 </div>
                             );
                         })}
+                        </div>
+                        <div className="row">
+                            <CommentButton />
+                        </div>
                     </div>
             );
         }
@@ -54,6 +64,10 @@ function CampsiteInfo(props) {
                     <div className="row">
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments comments={props.comments} />
+                        
+                    </div>
+                    <div className="row">
+                        <CommentForm />
                     </div>
                 </div>
             );
@@ -61,7 +75,94 @@ function CampsiteInfo(props) {
     return (
         <div />
     );
+}
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            rating: '',
+            name: '',
+            comment: '',
+            touched: {
+                rating: false,
+                name: false,
+                comment: false,
+            }
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleSubmit(values) {
+        console.log("Current state is: " +JSON.stringify(values));
+        alert("Current state is: " +JSON.stringify(values));
+    }
+    
+    render() {
+        return (
+            <div className="container">
+                
+                    <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                        <Row className="form-group">
+                            <Col>
+                                <Label htmlFor=".rating">Rating</Label>
+                                <Control.select model=".rating" className="form-control" name="rating">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </Col>
+                        </Row>
+                        <Row className="form-group">
+                            <Col>
+                                <Label htmlFor=".name">Your Name</Label>
+                                <Control.text model=".name" className="form-control" id="name" name="name" placeholder="Your Name" />
+                            </Col>
+                        </Row>
+                        <Row className="form-group">
+                            <Col>
+                                <Label htmlFor="comment">Comment</Label>
+                                <Control.textarea model=".feedback" className="form-control" id="feedback" name="feedback" rows="6" />
+                            </Col>
+                        </Row>
+                        <Row className="form-group">
+                            <Col>
+                                <Button type="submit" color="primary">Submit</Button>
+                            </Col>
+                        </Row>
+                    </LocalForm>
+            </div>
+        );
+    }
+}
+
+class CommentButton extends Component {
+    render() {
+        return (
+            <Button outline><i class="fa fa-pencil"></i> Submit Comment</Button>
+        );
+    }
+}
+
+class CommentModal extends Component {
+
+
+    render() {
+        return (
+            <div>
+            <Modal>
+                <ModalHeader>Submit Comment</ModalHeader>
+                <ModalBody>
+                    <CommentForm />
+                </ModalBody>
+            </Modal>
+            </div>
+        );
+    }
+}
 
 
 export default CampsiteInfo;
