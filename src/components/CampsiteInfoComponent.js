d2ed063bc6c71603b228a4b1188b146e13758903
Nathan Chuluda <nchuluda@gmail.com>
2020-01-18 12:23:13 -0500

    Task 3 Complete

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, ModalFooter, Col, Row, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Control, LocalForm } from 'react-redux-form';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
-
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
 
 function RenderCampsite({campsite}) {
         return(
@@ -130,13 +131,27 @@ class CommentForm extends Component {
                             <Row className="form-group">
                                 <Col>
                                     <Label htmlFor=".name">Your Name</Label>
-                                    <Control.text model=".name" className="form-control" id="name" name="name" placeholder="Your Name" />
+                                    <Control.text model=".name" className="form-control" id="name" name="name" placeholder="Your Name"
+                                        validators={{
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15)
+                                        }} />
+                                        <Errors 
+                                        className="text-danger"
+                                        model=".name"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
                                 </Col>
                             </Row>
                             <Row className="form-group">
                                 <Col>
                                     <Label htmlFor="comment">Comment</Label>
-                                    <Control.textarea model=".feedback" className="form-control" id="feedback" name="feedback" rows="6" />
+                                    <Control.textarea model=".comment" className="form-control" id="comment" name="comment" rows="6" />
                                 </Col>
                             </Row>
                             <Row className="form-group">
