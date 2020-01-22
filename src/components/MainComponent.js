8883a7df1d239504332b22a7f16e7819bddedd83
Nathan Chuluda <nchuluda@gmail.com>
2020-01-22 12:48:49 -0500

    React Redux Forms Revisited

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,8 @@ import Contact from './ContactComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { addComment, fetchCampsites } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form'
+;import { addComment, fetchCampsites } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -21,7 +22,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = {
     addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
-    fetchCampsites: () => (fetchCampsites())
+    fetchCampsites: () => (fetchCampsites()),
+    resetFeedbackForm: () => (actions.reset('feedbackForm'))
 };
 
 class Main extends Component {
@@ -61,7 +63,8 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} /> 
                     <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-                    <Route exact path='/contactus' component={ Contact } />
+                    <Route exact path='/contactus' render={() => <Contact resetFeedbackForm=
+                    {this.props.resetFeedbackForm} /> } />
                     <Route exact path='/about' render={() => <About partners={this.props.partners} />} />
                     <Redirect to='/home' />
 
