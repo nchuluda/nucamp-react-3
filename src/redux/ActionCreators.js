cc17f4006f91aa6aed299d864103cb6f33a01ce5
Nathan Chuluda <nchuluda@gmail.com>
2020-01-25 13:59:14 -0500

    task 2.0

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -186,42 +186,32 @@ export const addPartner = partners => ({
     payload: partners
 });
 
-export const postFeedback = ({firstName, lastName, phoneNum, email, agree, contactType, feedback}) => dispatch => {
-    
-    const newFeedback = {
-        firstName: firstName,
-        lastName: lastName,
-        phoneNum: phoneNum,
-        email: email,
-        agree: agree,
-        contactType: contactType,
-        feedback: feedback
-    };
-
-    
-
+export const postFeedback = (feedback) => () => {
     return fetch(baseUrl + 'feedback', {
-            method: "POST",
-            body: JSON.stringify(newFeedback),
-            headers: {
-                "Content-Type": "application/json"
+        method: "POST",
+        body: JSON.stringify(feedback),
+        headers: {
+          "Content-Type": "application/json"
+        },
+    })
+    .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                error.response = response;
+                throw error;
             }
-        })
-        .then(response => {
-                if (response.ok) {
-                    return response;
-                } else {
-                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
-                    error.response = response;
-                    throw error;
-                }
             },
             error => { throw error; }
         )
         .then(response => response.json())
-        .then(response => alert("Thank you for your feedback." + newFeedback))
-        .catch(error => {
-            console.log('post feedback', error.message);
+        .then(response => {
+            console.log('Feedback: ', response);
+            alert('Thank you for your feedback!\n' + JSON.stringify(response));
+        })
+        .catch(error =>  {
+            console.log('Feedback: ', error.message);
             alert('Your feedback could not be posted\nError: ' + error.message);
         });
 };
